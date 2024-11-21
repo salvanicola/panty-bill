61cfa7219e2363ed1e5bfb56c4c282dae8d73929
Nicola Salvadore <nicola.salvadore@gmail.com>
2024-11-21 23:46:40 +0100

    feat: authentication mvp

---
+++ lib/app_router.dart
@@ -0,0 +1,60 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:go_router/go_router.dart';
+import 'package:panty_bill/src/authentication/pages/sign_in_page.dart';
+import 'package:panty_bill/src/home/pages/home_page.dart';
+
+class AppRouter {
+  // Add GoRouter configuration outside the App class
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+          path: '/',
+          name: 'home',
+          builder: (context, state) => HomePage(),
+          redirect: (context, state) {
+            var isAuthenticated = FirebaseAuth.instance.currentUser != null;
+
+            if (isAuthenticated) {
+              return null;
+            }
+            return '/sign-in';
+          },
+          routes: [
+            GoRoute(
+              name: 'profile',
+              path: '/profile',
+              builder: (context, state) {
+                return ProfileScreen(
+                  providers: const [],
+                  actions: [
+                    SignedOutAction((context) {
+                      context.pushReplacement('/');
+                    }),
+                  ],
+                );
+              },
+            ),
+          ]),
+      GoRoute(
+        name: 'sign-in',
+        path: '/sign-in',
+        builder: (context, state) => SignInPage(),
+        routes: [
+          GoRoute(
+            name: 'forgot-password',
+            path: '/forgot-password',
+            builder: (context, state) {
+              final arguments = state.uri.queryParameters;
+              return ForgotPasswordScreen(
+                email: arguments['email'],
+                headerMaxExtent: 200,
+              );
+            },
+          ),
+        ],
+      ),
+    ],
+  );
+// end of GoRouter configuration
+}
---
+++ lib/src/authentication/pages/sign_in_page.dart
@@ -0,0 +1,52 @@
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final providers = [
+      EmailAuthProvider(),
+    ];
+
+    return SignInScreen(
+      providers: providers,
+      actions: [
+        ForgotPasswordAction(((context, email) {
+          final uri = Uri(
+            path: '/sign-in/forgot-password',
+            queryParameters: <String, String?>{
+              'email': email,
+            },
+          );
+          context.push(uri.toString());
+        })),
+        AuthStateChangeAction(((context, state) {
+          final user = switch (state) {
+            SignedIn state => state.user,
+            UserCreated state => state.credential.user,
+            _ => null
+          };
+          if (user == null) {
+            return;
+          }
+          if (state is UserCreated) {
+            user.updateDisplayName(user.email!.split('@')[0]);
+          }
+          if (!user.emailVerified) {
+            user.sendEmailVerification();
+            const snackBar = SnackBar(
+                content: Text(
+                    'Please check your email to verify your email address'));
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          }
+          context.pushReplacement('/');
+        })),
+      ],
+    );
+  }
+}
